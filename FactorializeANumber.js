ebcfb4ed5a53faa0d0e43d37ff2e85c9d7fae032
Dan Valeroso <danlestervaleroso@gmail.com>
2020-08-13 21:44:37 +0800

    Initial Commit

---
+++ FactorializeANumber.js
@@ -0,0 +1,11 @@
+function factorialize(num) {
+    let newNum = 1;
+    for(let i = num; i > 0; i--){
+      newNum *= i;
+      
+    }
+    return newNum;
+  }
+  
+  factorialize(5);
+  
---
+++ ReverseAString.js
@@ -0,0 +1,13 @@
+function reverseString(str) {
+    let regex = /[\w\s]/g;
+    let arr = str.match(regex);
+    let newStr = "";
+    for (let i = arr.length - 1; i > -1; i--){
+      newStr += arr[i];
+    }
+    str = newStr;
+    return str;
+  }
+  
+  reverseString("hello");
+  
